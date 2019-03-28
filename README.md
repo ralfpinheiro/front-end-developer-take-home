f2bad682ad36fd51d5c6175f0304788e0c675883
Alex MacKenzie <alexbcmac7@gmail.com>
2019-03-28 10:18:10 -0700

    Setup project

--- README.md
+++ README.md
@@ -1,29 +1,40 @@
-# vue-front-end-take-home
+Trackloop's Front End SPA Project
+===
+
+Hey Front End developer. Welcome. Your mission, should you choose to accept it, is to carve out 2 hours and create a single page app (SPA) using the mini TuneIn station API defined below.
+
+> **Hey this is important!**
+> We hope you can spend about two hours on this project. If you can finish faster -- great! If not, limit yourself and don't spend much longer than 2 hours MAX.
+
+# Your Tasks
+
+* We've provided code for you to get up and running fast!  We encourage you to use it if you think it will help, but feel free to roll your own solution if you like.
+* Be creative with this, when in doubt, make an executive decision. *Functionality is more important than the look and feel*. If you finish early, feel free to polish it up.
+* The App page shows list of vehicles in an arbitrary company's fleet, with buttons to CREATE a new vehicle that will be added to the list and a button to DELETE a specific vehicle from the list.
+* Your tasks are as follows,
+    * Dynamically increment the index for the vehicle before adding it to the vehicles object
+    * Clear the form after the vehicle has been added to the vehicles object
+    * Refactor the App so that two new components handle the table listing the vehicles and the form handling adding a new vehicle
+    * Implement delete vehicle functionality
+* There are `TODO`'s inline to help direct you with the tasks
+* [Bootstrap 4](https://getbootstrap.com/docs/4.0/getting-started/introduction/) is being used for styling and has already been imported.
+* Once finished, make a pull request and tag it for review
+
+## To Get Started
+
+* Fork the repository
+* Clone the repository
+* Create a new branch for your task
+* Commit your changes to that branch
+* Make a pull request to our repository and mark it for review
 
 ## Project setup
+
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+##### Compiles and hot-reloads for development
 ```
 npm run serve
 ```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,111 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="container">
+        <div class="row">
+            <div class="col-12 my-5 p-0">
+                <h1>Fleet Assets</h1>
+            </div>
+            <!-- TODO: Separate into a new component -->
+            <div class="col-12 p-0">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Year</th>
+                        <th scope="col">Make</th>
+                        <th scope="col">Model</th>
+                        <th scope="col">Colour</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="(vehicle, index) in vehicles" :key="index">
+                        <td> {{ vehicle.year }} </td>
+                        <td> {{ vehicle.make }} </td>
+                        <td> {{ vehicle.model }} </td>
+                        <td> {{ vehicle.colour }} </td>
+                        <td>
+                            <!-- TODO: Implement deleting the specified vehicle -->
+                            <button type="button" class="btn btn-sm btn-outline-danger mx-2">Delete</button>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <!-- TODO: Separate into a new component-->
+        <form class="mt-4">
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Year" v-model="form.year">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Make" v-model="form.make">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Model" v-model="form.model">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Colour" v-model="form.colour">
+                </div>
+                <div class="col">
+                    <button type="button" class="btn btn-primary mx-2" @click="create">Create</button>
+                    <button type="reset" class="btn btn-danger mx-2">Reset</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        name: 'app',
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+        data() {
+            return {
+                vehicles: {
+                    1: {
+                        year: '2018',
+                        make: 'Ford',
+                        model: 'Econoline',
+                        colour: 'White',
+                    },
+
+                    2: {
+                        year: '2017',
+                        make: 'Mercedes',
+                        model: 'Sprinter',
+                        colour: 'Black',
+                    }
+                },
+
+                form: {
+                    year: '',
+                    make: '',
+                    model: '',
+                    colour: '',
+                }
+            }
+        },
+
+        methods: {
+            create() {
+                // TODO: Dynamically increment the Index based on the number of vehicles
+                const index = 3;
+
+                this.$set(this.vehicles, index, this.form);
+
+                // TODO: Reset the form
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 </style>
